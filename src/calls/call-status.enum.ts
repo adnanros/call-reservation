a82ce2b43f77a9fdd38af8ab6b5b2d093c2d66b5
adnan <adnan.ros1@gmail.com>
2022-03-23 16:14:36 +0430

    feat: add CallsModule structure (including Call-Entity and its Status)

---
+++ src/calls/call-status.enum.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export enum CallStatus {
+    REQUESTED = 'REQUESTED',
+    ACCEPTED = 'ACCEPTED',
+    REJECTED = 'REJECTED',
+    CANCELED = 'CANCELED',
+  }
+  
