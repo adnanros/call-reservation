887715e475725b907b59c842c917f4b686e8bb2d
adnan <adnan.ros1@gmail.com>
2022-03-25 13:29:45 +0430

    test: add getSingleReservationResponse in service testing

--- src/reservations/reservations.service.spec.ts
+++ src/reservations/reservations.service.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ReservationStatus } from './reservation-status.enum';
+import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
 
+const mockReservationRepository = () => ({
+  getReservationsresponse: jest.fn(),
+  findOne: jest.fn(),
+});
+
 describe('CallsService', () => {
-  let service: ReservationsService;
+  let reservationService: ReservationsService;
+  let reservationRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReservationsService],
+      providers: [
+        ReservationsService,
+        {
+          provide: ReservationsRepository,
+          useFactory: mockReservationRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<ReservationsService>(ReservationsService);
+    reservationService = module.get<ReservationsService>(ReservationsService);
+    reservationRepository = module.get(ReservationsRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(reservationService).toBeDefined();
+  });
+
+  describe('getSingleReservationResponse', () => {
+    it('calls reservationRepository.findOne and return a result', async () => {
+      const mockReservation = {
+        id: 'someId',
+        startTime: 'someTime',
+        endTime: 'someTime',
+        email: 'someEmail',
+        phone: 'phoneNumber',
+        pushNotificationKey: 'someKey',
+        receiveEmail: false,
+        receiveSmsNotification: false,
+        receivePushNotification: false,
+        createdTime: 'someTime',
+        status: ReservationStatus.REQUESTED,
+      };
+      reservationRepository.findOne.mockResolvedValue(mockReservation);
+      const result = await reservationService.getSingleReservationResponse(
+        'someId',
+      );
+      expect(result).toEqual(mockReservation);
+    });
   });
 });
